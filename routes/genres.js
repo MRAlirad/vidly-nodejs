a13ef9c29481e77d9be11d9db0a7618b82aefd82
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-06-30 00:34:58 +0330

    modifying genre model

--- routes/genres.js
+++ routes/genres.js
@@ -1,23 +1,17 @@
+const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 50,
-    }
-}));
+
 
 router.get('/', async (req, res) => {
     res.send(await Genre.find().sort('name'));
 });
 
 router.post('/', async (req, res) => {
-    const {error} = validateGenre(req.body);
+    const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let genre = new Genre({name: req.body.name});
@@ -26,7 +20,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-    const {error} = validateGenre(req.body);
+    const {error} = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name}, {
@@ -39,7 +33,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-    const genre = await Genre.findByIdAndDelete(req.params.id)
+    const genre = await Genre.findByIdAndDelete(req.params.id);
 
     if (!genre) return res.status(404).send('The genre with the given ID not found');
 
@@ -54,12 +48,6 @@ router.get('/:id', async (req, res) => {
     res.send(genre);
 });
 
-const validateGenre = (genre) => {
-    const schema = Joi.object({
-        name: Joi.string().min(5).required(),
-    });
 
-    return schema.validate(genre);
-};
 
 module.exports = router;

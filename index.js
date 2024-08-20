bfabc926cb4a77ec9575c887bc7c1b9531d5ac26
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-08-20 21:53:33 +0330

    Authenticating user

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
@@ -18,6 +19,7 @@ app.use('/api/movies', movies);
 app.use('/api/customers', customers);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 
